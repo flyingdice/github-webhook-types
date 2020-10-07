b7a859eba4592672ba5d2428badd3d7864249586
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-07 12:00:08 -0700

    Export mappings from hooks sub-package
    These mappings store EventName -> ID/Event class types so we can
    create the appropriate class based on the webhook event.

--- ghwht/hooks/__init__.py
+++ ghwht/hooks/__init__.py
@@ -4,3 +4,39 @@
 
     Contains modules for each type of webhook event.
 """
+from typing import Dict, Type
+
+from . import base, installation, ping, pull_request, repository
+
+__all__ = ['NAME_TO_EVENT', 'Event', 'EventName', 'EventT', 'ID']
+
+# Alias to simplify imports.
+Event = base.Event
+EventName = base.EventName
+EventT = base.EventT
+ID = base.ID
+IDT = base.IDT
+
+
+def hooks_modules():
+    """
+    Generator function that yields all registered webhook event modules.
+    """
+    for module in (installation, ping, pull_request, repository):
+        yield module
+
+
+# Lookup table that maps event names to their appropriate identifier type.
+# Ex: 'issues' -> ghwht.hooks.issues.Identifier.
+NAME_TO_ID: Dict[EventName, Type[IDT]] = dict(
+    (module.Name, module.ID)
+    for module in hooks_modules()
+)
+
+
+# Lookup table that maps event names to their appropriate type.
+# Ex: 'issues' -> ghwht.hooks.issues.Event.
+NAME_TO_EVENT: Dict[EventName, Type[EventT]] = dict(
+    (module.Name, module.Event)
+    for module in hooks_modules()
+)
