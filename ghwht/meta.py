3c1d5f31cc377a8e8a0cea0835586fe8805bc385
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-06 19:34:54 -0700

    Add ghwht package and setup meta vars

---
+++ ghwht/meta.py
@@ -0,0 +1,14 @@
+"""
+    ghwht/meta
+    ~~~~~~~~~
+
+    Contains information about the package.
+"""
+AUTHOR_NAME = 'Andrew Hawker'
+AUTHOR_EMAIL = 'andrew.r.hawker@gmail.com'
+AUTHOR = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
+NAME = 'ghwht'
+VERSION = '0.0.1'
+TAGLINE = 'Type definitions for GitHub webhooks.'
+URL = 'https://github.com/flyingdice/github-webhook-types'
+LICENSE = 'Apache 2.0'
