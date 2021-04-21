1efcdd9cde97832f4187308dc3b862f9928a547f
Andrew Hawker <andrew.r.hawker@gmail.com>
2021-04-20 20:51:18 -0700

    Update installation event to handle multiple single file paths

--- ghwht/hooks/installation.py
+++ ghwht/hooks/installation.py
@@ -30,14 +30,16 @@ class Installation:
     app_slug: str
     created_at: datetime
     events: List[base.EventName]
+    has_multiple_single_files: Optional[bool]
     html_url: HttpUrl
     id: int
     permissions: common.InstallationPermissions
     repository_selection: common.RepositorySelection
     repositories_url: HttpUrl
     single_file_name: Optional[str]
-    suspended_at: Optional[str]
-    suspended_by: Optional[str]
+    single_file_paths: List[str]
+    suspended_at: Optional[datetime]
+    suspended_by: Optional[common.User]
     target_id: int
     target_type: common.TargetType
     updated_at: datetime
